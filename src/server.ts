f67ac9f30c965a2e38d8db4e6884844b88e974ca
ahmadnusa <ahmadnursaefullah79@gmail.com>
2024-02-23 18:57:20 +0700

    Listening for stripe webhook locally

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { inferAsyncReturnType } from "@trpc/server"
 import * as trpcExpress from "@trpc/server/adapters/express"
+import bodyParser from "body-parser"
 import express from "express"
+import { IncomingMessage } from "http"
 
 import { getPayloadClient } from "./get-payload"
 import { nextApp, nextHandler } from "./next-utils"
 import { appRouter } from "./trpc"
+import { stripeWebhookHandler } from "./webhooks"
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -19,7 +22,19 @@ const createContext = ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer
+}
+
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer
+    },
+  })
+
+  app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler)
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
